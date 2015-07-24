55d42801923efa16cb4d5c09a1dd0eec99ccdf55
trung <trung@mdkt.org>
2015-07-24 22:34:58 +0800

    customize error page and error attributes

--- gateway/src/main/java/org/demo/gateway/GatewayApplication.java
+++ gateway/src/main/java/org/demo/gateway/GatewayApplication.java
@@ -1,11 +1,15 @@
 package org.demo.gateway;
 
 import java.security.Principal;
+import java.util.Map;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.boot.autoconfigure.web.DefaultErrorAttributes;
+import org.springframework.boot.autoconfigure.web.ErrorAttributes;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,6 +18,7 @@ import org.springframework.security.web.header.writers.frameoptions.XFrameOption
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter.XFrameOptionsMode;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestAttributes;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -27,6 +32,20 @@ public class GatewayApplication {
 			@Override
 			public String getName() {
 				return "Demo";
+			}
+		};
+	}
+	
+	@Bean
+	public ErrorAttributes errorAttributes() {
+		return new DefaultErrorAttributes() {
+			@Override
+			public Map<String, Object> getErrorAttributes(
+					RequestAttributes requestAttributes,
+					boolean includeStackTrace) {
+				Map<String, Object> errorAttributes = super.getErrorAttributes(requestAttributes, includeStackTrace);
+				errorAttributes.put("error", "ABC-123");
+				return errorAttributes;
 			}
 		};
 	}
